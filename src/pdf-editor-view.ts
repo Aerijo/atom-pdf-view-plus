fc4a16a19da87dfc08f0c6b7984a9cf79ba1cb7f
Benjamin Gray <bengray178@gmail.com>
2019-08-26 15:57:20 +1000

    simplify frame element

--- src/pdf-editor-view.ts
+++ src/pdf-editor-view.ts
@@ -3,12 +3,16 @@ import PdfEditor from './pdf-editor';
 
 export default class PdfEditorView {
   element: any;
-  frame: any;
   editor: PdfEditor;
 
   constructor(editor: PdfEditor) {
     this.editor = editor;
-    this.element = document.createElement("div");
+    const frame = document.createElement("iframe");
+    frame.setAttribute("id", "pdf-frame");
+    frame.setAttribute("width", "100%");
+    frame.setAttribute("height", "100%");
+    frame.setAttribute("frameborder", "0");
+    this.element = frame;
     this.update();
   }
 
@@ -17,22 +21,8 @@ export default class PdfEditorView {
   }
 
   update() {
-    const frame = document.createElement("iframe");
-    frame.setAttribute("id", "pdf-frame");
-    frame.setAttribute("width", "100%");
-    frame.setAttribute("height", "100%");
-    frame.setAttribute("frameborder", "0");
-
     const src = `${path.join(__dirname, "..", "vendor", "pdfjs", "web", "viewer.html")}?file=${encodeURIComponent(this.filePath)}`;
-    frame.setAttribute("src", src);
-
-    if (this.frame) {
-      this.element.replaceChild(frame, this.frame);
-    } else {
-      this.element.appendChild(frame);
-    }
-
-    this.frame = frame;
+    this.element.setAttribute("src", src);
   }
 
   destroy() {
