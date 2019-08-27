b3ed897a2c5fe3c5049ada064b08dafdc6b2b32f
Benjamin Gray <bengray178@gmail.com>
2019-08-27 10:19:30 +1000

    comment out click handler

--- vendor/pdfjs/custom/latexscript.js
+++ vendor/pdfjs/custom/latexscript.js
@@ -1,7 +1,7 @@
-console.log(this)
+// console.log(this)
 this.onclick = (e) => {
-  console.log("clicked", e)
-  PDFViewerApplication.pdfDocument.getPage(1).then(page => {
-    console.log(page.getViewport())
-  })
+  // console.log("clicked", e)
+  // PDFViewerApplication.pdfDocument.getPage(1).then(page => {
+  //   console.log(page.getViewport())
+  // })
 }
