3294acc1365bc6ebcc6e672a913db955c3699bc3
Benjamin Gray <bengray178@gmail.com>
2019-09-13 15:31:37 +1000

    wait for frame to load before setting options

--- vendor/pdfjs/custom/latexscript.js
+++ vendor/pdfjs/custom/latexscript.js
@@ -1,13 +1,21 @@
-console.log(this)
-
-PDFViewerApplicationOptions.set("sidebarViewOnLoad", 0);
-PDFViewerApplicationOptions.set("enableWebGL", true);
-PDFViewerApplicationOptions.set("externalLinkTarget", 4);
-PDFViewerApplicationOptions.set("eventBusDispatchToDOM", true);
+window.onload = () => {
+  PDFViewerApplicationOptions.set("sidebarViewOnLoad", 0);
+  PDFViewerApplicationOptions.set("enableWebGL", true);
+  PDFViewerApplicationOptions.set("externalLinkTarget", 4);
+  PDFViewerApplicationOptions.set("eventBusDispatchToDOM", true);
+}
 
 window.addEventListener("message", event => {
   const data = event.data;
-  if (data.source) {
-    PDFViewerApplication.open(message.data.source);
+  switch (data.type) {
+    case "refresh":
+      refreshContents(data.source);
+      return;
+    default:
+      throw new Error(`Unexpected message type ${data.type} received`);
   }
 });
+
+function refreshContents(filepath) {
+  PDFViewerApplication.open(filepath);
+}
